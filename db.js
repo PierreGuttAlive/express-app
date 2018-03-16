f1efe478bfebc9f62d86483a421c31ad5b18b168
Ivan <iwan.ku96@yandex.ru>
2018-03-17 02:38:06 +0300

    added safe db connection

--- db.js
+++ db.js
@@ -1,4 +1,5 @@
+require("dotenv").config();
 var monk = require("monk");
-var db = monk('localhost:27017/express_app');
+var db = monk(`${process.env.DB_USER}:${process.env.DB_PASS}@localhost:27017/${process.env.DB}`);
 
 module.exports = db;
